ea8206bdbe60a31f66bde240c1f9b41cf8a507fc
louisleedev <bj.louislee@gmail.com>
2023-10-27 23:27:47 -0400

    Nav.js changed

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,126 +1,139 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
-
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+
   const [providers, setProviders] = useState(null);
-  const [toggleDropdown, setToggleDropdown] = useState(false)
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
-      const response = await getProviders();
-
-      setProviders(response);
-    }
-    setProviders();
-  }, [])
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href="/" className='flex gap-2 flex-center'>
-        <Image src="assets/images/logo.svg"
-          alt="Promptopia Logo"
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+          src='/assets/images/logo.svg'
+          alt='logo'
           width={30}
           height={30}
-          className='object-contain' />
-        <p className="logo_text">Promptopia</p>
+          className='object-contain'
+        />
+        <p className='logo_text'>Promptopia</p>
       </Link>
 
       {/* Desktop Navigation */}
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex gap-3 md:gap-5'>
-            <Link href="/create-prompt" className='black_btn'>
+            <Link href='/create-prompt' className='black_btn'>
               Create Post
             </Link>
-            <button type="button" onClick={signOut} className='outline_btn'>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
               Sign Out
             </button>
 
             <Link href='/profile'>
-              <Image src="/assets/images/logo.svg"
+              <Image
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
-                alt="profile"
+                alt='profile'
               />
             </Link>
           </div>
         ) : (
           <>
             {providers &&
-              Object.values(providers).map((provider) =>
-              (
+              Object.values(providers).map((provider) => (
                 <button
                   type='button'
                   key={provider.name}
-                  onClick={() => signIn(provider.id)}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
                   className='black_btn'
                 >
-                  Sign In
+                  Sign in
                 </button>
               ))}
-          </>)}
+          </>
+        )}
       </div>
 
       {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className='flex'>
-            <Image src="/assets/images/logo.svg"
+            <Image
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
-              alt="profile"
-              onClick={() => setToggleDropdown((prev) => !prev)}
+              alt='profile'
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
+
             {toggleDropdown && (
               <div className='dropdown'>
                 <Link
-                  href="/profile"
+                  href='/profile'
                   className='dropdown_link'
-                  onClick={() => setToggleDropdown(false)}>
+                  onClick={() => setToggleDropdown(false)}
+                >
                   My Profile
                 </Link>
                 <Link
-                  href="/create-prompt"
+                  href='/create-prompt'
                   className='dropdown_link'
-                  onClick={() => setToggleDropdown(false)}>
+                  onClick={() => setToggleDropdown(false)}
+                >
                   Create Prompt
                 </Link>
-                <button type='button'
+                <button
+                  type='button'
                   onClick={() => {
-                    setToggleDropdown(false)
+                    setToggleDropdown(false);
                     signOut();
                   }}
-                  className='mt-5 w-full black_btn'>
+                  className='mt-5 w-full black_btn'
+                >
                   Sign Out
                 </button>
               </div>
             )}
           </div>
-        ) : (<>
-          {providers &&
-            Object.values(providers).map((provider) =>
-            (
-              <button
-                type='button'
-                key={provider.name}
-                onClick={() => signIn(provider.id)}
-                className='black_btn'
-              >
-                Sign In
-              </button>
-            ))}
-        </>)}
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => {
+                    signIn(provider.id);
+                  }}
+                  className='black_btn'
+                >
+                  Sign in
+                </button>
+              ))}
+          </>
+        )}
       </div>
-    </nav >
-  )
-}
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
