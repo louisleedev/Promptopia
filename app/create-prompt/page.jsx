bd374c4ec7a3623473afb7647e598038d474f443
louisleedev <bj.louislee@gmail.com>
2023-11-06 22:15:18 -0500

    Able to update new prompt to MongoDB

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,40 +2,43 @@
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
- const [submitting, setSubmitting] = useState(false)
- const [post, setPost] = useState({
-  prompt: '',
-  tag: '',
- });
+  const router = useRouter();
+  const { data: session } = useSession();
 
- const createPrompt = async (e) => {
+  const [submitting, setSubmitting] = useState(false)
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
     try {
       const response = await fetch('/api/prompt/new',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session?.user.id,
-          tag: post.tag
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post.prompt,
+            userId: session?.user.id,
+            tag: post.tag
+          })
         })
-      })
-      if(response.ok){
+      if (response.ok) {
         router.push('/');
-    }
-    } catch (error){
+      }
+    } catch (error) {
       console.log(error);
     } finally {
       setSubmitting(false);
     }
- }
+  }
 
   return (
     <Form
